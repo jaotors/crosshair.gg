86203151929e8daff954df91172488ce60a39b48
Joshua B. Turingan <turingan.joshua@gmail.com>
2021-07-13 21:19:43 +0800

    Removes console log on next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const withImages = require('next-images')
 module.exports = withImages({
   reactStrictMode: true,
   webpack: (config) => {
-    const found = config.module.rules.findIndex((rule) => console.log(rule.test) || rule.test?.exec('u.svg'));
-    config.module.rules[found].test = /\.(jpe?g|png|gif)$/i;
+    const found = config.module.rules.findIndex((rule) =>
+      rule.test?.exec('u.svg')
+    )
+    config.module.rules[found].test = /\.(jpe?g|png|gif)$/i
 
     config.module.rules.push({
       test: /\.svg$/,
